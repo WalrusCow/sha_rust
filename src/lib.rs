98d4a8cb3db345023d262ccd53f93b1709ffeadf
WalrusCow <willgmcd@gmail.com>
2016-08-07 22:43:51 +0100

    It appears to actually work...

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,1 @@
 pub mod sha;
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
--- src/sha.rs
+++ src/sha.rs
@@ -1,3 +1,5 @@
+use std::num::Wrapping;
+
 fn rot_r(val: u32, rot: u32) -> u32 {
     assert!(rot < 32);
     (val >> rot) | (val << (32 - rot))
@@ -27,33 +29,34 @@ fn l_sigma_1(x: u32) -> u32 {
     rot_r(x, 17) ^ rot_r(x, 19) ^ (x >> 10)
 }
 
+// Intial hash state
+const INITIAL_HASH_STATE: [u32; 8] = [
+  0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+  0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
+];
+
 const K: [u32; 64] = [
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
     0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
     0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-
     0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
     0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-
     0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
     0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-
     0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
     0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-
     0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
     0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-
     0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
     0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
     0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
 ];
 
-struct Sha256Digestion {
+const BLOCK_SIZE_BYTES: usize = 64;
+
+pub struct Sha256Digestion {
     // Current state of the hash
     hash_state: [u32; 8],
     // This is the block we will read the inputs into.
@@ -61,17 +64,12 @@ struct Sha256Digestion {
     reading_block: Vec<u8>,
     // Number of bits in the message
     bit_count: u64,
-};
-
-const BLOCK_SIZE_BYTES = 64;
+}
 
 impl Sha256Digestion {
-    fn new() -> Sha256Digestion {
+    pub fn new() -> Sha256Digestion {
         Sha256Digestion {
-            hash_state: [
-              0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
-              0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
-            ],
+            hash_state: INITIAL_HASH_STATE,
             reading_block: Vec::with_capacity(BLOCK_SIZE_BYTES),
             bit_count: 0,
         }
@@ -91,22 +89,26 @@ impl Sha256Digestion {
         }
 
         let mut W: [u32; 64] = [0; 64];
-        let (mut a, mut b) = (self.hash_state[0], self.hash_state[1]);
-        let (mut c, mut d) = (self.hash_state[2], self.hash_state[3]);
-        let (mut e, mut f) = (self.hash_state[4], self.hash_state[5]);
-        let (mut g, mut h) = (self.hash_state[6], self.hash_state[7]);
+        let mut a: Wrapping<u32> = Wrapping(self.hash_state[0]);
+        let mut b: Wrapping<u32> = Wrapping(self.hash_state[1]);
+        let mut c: Wrapping<u32> = Wrapping(self.hash_state[2]);
+        let mut d: Wrapping<u32> = Wrapping(self.hash_state[3]);
+        let mut e: Wrapping<u32> = Wrapping(self.hash_state[4]);
+        let mut f: Wrapping<u32> = Wrapping(self.hash_state[5]);
+        let mut g: Wrapping<u32> = Wrapping(self.hash_state[6]);
+        let mut h: Wrapping<u32> = Wrapping(self.hash_state[7]);
 
         for (i, m) in M.iter().enumerate() {
-            W[i] = m;
+            W[i] = *m;
         }
         for t in 16..64 {
-            W[t] = l_sigma_1(W[t-2]) + W[t-7] + l_sigma_0(W[t-15]) + W[t-16];
+            W[t] = (Wrapping(l_sigma_1(W[t-2])) + Wrapping(W[t-7]) + Wrapping(l_sigma_0(W[t-15])) + Wrapping(W[t-16])).0;
         }
 
         // TODO: Use zip(K, W)
         for t in 0..64 {
-            let T1 = h + b_sigma_1(e) + ch(e, f, g) + K[t] + W[t];
-            let T2 = b_sigma_0(a) + maj(a, b, c);
+            let T1 = h + Wrapping(b_sigma_1(e.0)) + Wrapping(ch(e.0, f.0, g.0)) + Wrapping(K[t]) + Wrapping(W[t]);
+            let T2 = Wrapping(b_sigma_0(a.0)) + Wrapping(maj(a.0, b.0, c.0));
             h = g;
             g = f;
             f = e;
@@ -117,14 +119,14 @@ impl Sha256Digestion {
             a = T1 + T2;
         }
 
-        self.hash_state[0] += a;
-        self.hash_state[1] += b;
-        self.hash_state[2] += c;
-        self.hash_state[3] += d;
-        self.hash_state[4] += e;
-        self.hash_state[5] += f;
-        self.hash_state[6] += g;
-        self.hash_state[7] += h;
+        self.hash_state[0] = (Wrapping(self.hash_state[0]) + a).0;
+        self.hash_state[1] = (Wrapping(self.hash_state[1]) + b).0;
+        self.hash_state[2] = (Wrapping(self.hash_state[2]) + c).0;
+        self.hash_state[3] = (Wrapping(self.hash_state[3]) + d).0;
+        self.hash_state[4] = (Wrapping(self.hash_state[4]) + e).0;
+        self.hash_state[5] = (Wrapping(self.hash_state[5]) + f).0;
+        self.hash_state[6] = (Wrapping(self.hash_state[6]) + g).0;
+        self.hash_state[7] = (Wrapping(self.hash_state[7]) + h).0;
     }
 
     /// Produce the final hash value
@@ -149,7 +151,7 @@ impl Sha256Digestion {
         // TODO: This might also be the wrong way on
         // Now push on the length
         for i in 0..8 {
-            self.reading_block.push(self.bit_count & (0xff << (7 - i) * 8));
+            self.reading_block.push((self.bit_count >> ((7 - i) * 8)) as u8);
         }
         self.update_hash_state();
         self.reading_block.clear();
@@ -174,13 +176,18 @@ impl Sha256Digestion {
     // Any more?
 }
 
-let mut H: [u32; 8] = [
-  0x6a09e667,
-  0xbb67ae85,
-  0x3c6ef372,
-  0xa54ff53a,
-  0x510e527f,
-  0x9b05688c,
-  0x1f83d9ab,
-  0x5be0cd19,
-];
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut s = Sha256Digestion::new();
+        s.add_byte(97);
+        let h = s.digest();
+        for w in h.iter() {
+            print!("{:x}", w);
+        }
+        println!("");
+    }
+}
